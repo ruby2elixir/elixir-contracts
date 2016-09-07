1fd1094e6546019c8bbf2fede04213678d2f27ab
Roman Heinrich <roman.heinrich@gmail.com>
2016-09-07 13:20:16 +0200

    unit tests for disabled contracts

--- test/contracts_test.exs
+++ test/contracts_test.exs
@@ -28,20 +28,41 @@ defmodule ContractsTest do
     end
   end
 
-  describe "failing precondition" do
-    test "fill/1 function with a :closed in_valve raises" do
-      tank = %Tank{level: 10, in_valve: :closed}
-      assert_raise RuntimeError, "Precondition not met: not full?(tank) && tank.in_valve() == :open && tank.out_valve() == :closed", fn->
-        Tank.fill(tank)
+  if Contracts.Config.is_enabled? do
+    describe "failing precondition" do
+      test "fill/1 function with a :closed in_valve raises" do
+        tank = %Tank{level: 10, in_valve: :closed}
+        assert_raise RuntimeError, "Precondition not met: not full?(tank) && tank.in_valve() == :open && tank.out_valve() == :closed", fn->
+          Tank.fill(tank)
+        end
+      end
+    end
+
+    describe "failing postcondition" do
+      test "buggy empty/1 function does not correctly empty the tank" do
+        tank = %Tank{level: 10, out_valve: :open}
+        assert_raise RuntimeError, "Postcondition not met: empty?(result) && result.in_valve() == :closed && result.out_valve() == :closed", fn->
+          Tank.empty(tank)
+        end
       end
     end
   end
 
-  describe "failing postcondition" do
-    test "buggy empty/1 function does not correctly empty the tank" do
-      tank = %Tank{level: 10, out_valve: :open}
-      assert_raise RuntimeError, "Postcondition not met: empty?(result) && result.in_valve() == :closed && result.out_valve() == :closed", fn->
-        Tank.empty(tank)
+  unless Contracts.Config.is_enabled? do
+    describe "failing precondition" do
+      test "fill/1 function with a :closed in_valve raises" do
+        tank = %Tank{level: 9, in_valve: :closed}
+        tank = Tank.fill(tank)
+        # because pre-condition is disabled, the function executes and fills the tank...
+        assert Tank.full?(tank)
+      end
+    end
+    describe "failing postcondition is disabled" do
+      test "buggy empty/1 function does not correctly empty the tank" do
+        tank = %Tank{level: 10, out_valve: :open}
+        tank = Tank.empty(tank)
+        # because the function is buggy and post-condition is disabled, the tank is not really empty
+        refute Tank.empty?(tank)
       end
     end
   end
