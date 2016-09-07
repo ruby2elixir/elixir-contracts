e39067eec84d8be51b5cf5cc491e39dd48a71f8e
Roman Heinrich <roman.heinrich@gmail.com>
2016-09-07 19:29:58 +0200

    adds package

--- mix.exs
+++ mix.exs
@@ -9,6 +9,7 @@ defmodule Contracts.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package,
      deps: deps]
   end
 
